962965ea209dfd8933db08ce1afb7fb36e23c5d7
infernaL_wizarD <infernal_wizard@mail.ru>
2010-12-04 01:54:34 +0300

    13 commit Прилепил меню к основному контейнеру, нарисовал фоны к пунктам, внёс множество изменений в style.css, касательно расположений элементов и их вида. Удалил значок Home и перерисовал значок RSS.
    Signed-off-by: infernaL_wizarD <infernal_wizard@mail.ru>

--- www/themes/acoffee/tpl/page.tpl.php
+++ www/themes/acoffee/tpl/page.tpl.php
@@ -57,7 +57,7 @@
               <div id="name-and-slogan">
                 <?php if (!empty($site_name)): ?>
                   <p id="site-name">
-                    <a href="<?php print $front_page ?>" title="<?php print t('Home'); ?>" rel="home"></a>
+                    <a href="<?php print $front_page ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name ?></a>
                   </p>
                 <?php endif; ?>
 
@@ -75,7 +75,6 @@
 		  
           <?php if (!empty($rss_twit)): ?>
             <div id="rss-twit">
-			  <a href="<?php print $front_page ?>" title="<?php print t('Home'); ?>" rel="home"><?php print theme('image', drupal_get_path('theme', 'acoffee') ."/img/home.png") ?></a>
               <?php print $rss_twit; ?>
             </div>
           <?php endif; ?>
